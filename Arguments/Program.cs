7a580b18aa03c646f056052afcb1db443c9eadc9
Jerzy Los <jerzy.mar.los@dxc.com>
2021-03-19 18:17:49 +0100

    Fix Program.cs to have usage example.

--- Arguments/Program.cs
+++ Arguments/Program.cs
@@ -5,101 +5,27 @@ namespace Arguments
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            //-l - p 8080 - d / usr / logs
-
-            //-l -p 1 -d s
-            //string schmat = "-l -p 1 -d s";
-            string schmat = "-p 2-10 -d s-100";
-            string domylsne = "-p 777 -d super";
-            string[] schmatTablica = schmat.Split(' ', StringSplitOptions.None);
-
-
-            Dictionary<string, string> flagsSchema = CheckArgs(schmatTablica);
-            Dictionary<string, string> flags = CheckArgs(args);
-
-            foreach (string flag in flags.Keys)
+            try
             {
-                if (!flagsSchema.ContainsKey(flag))
-                {
-
-                    Console.WriteLine("Not expected flag: " + flag);
-                    Console.WriteLine("Usage: " + schmat);
-                    return -1;
-                } else
-                {
-                    if (flagsSchema[flag] != null)
-                    {
-                        //typ do sprawdzenia
-                        if (flags[flag] != null)
-                        {
-                            //wymaga i jest wartosc dla flagi sprawdzmay typ
-                            if (flagsSchema[flag] == "1")
-                            {
-                                int i;
-                                if (!(int.TryParse(flags[flag], out i))){
-                                    Console.WriteLine("Numeric argument expected for flag: " + flag);
-                                    Console.WriteLine("Usage: " + schmat);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            //nie podan
-                            //moze jest odmylne
-                            Console.WriteLine("Expected argument not provided for flag: " + flag);
-                            Console.WriteLine("Usage: " + schmat);
-                        }
-
-                    } else
-                    {
-                        //flags schema null
-                        if (flags[flag] != null)
-                        {
-                            Console.WriteLine("Not expected argument for flag: " + flag);
-                            Console.WriteLine("Usage: " + schmat);
-                        }
-
-                    }
-                }
-
-
+                Args arg = new Args("l,p#,d*", args);
+                bool logging = arg.getBool('l');
+                int port = arg.getInt('p');
+                String directory = arg.getString('d');
+                executeApplication(logging, port, directory);
+            }
+            catch (ArgsException e)
+            {
+                Console.WriteLine("Argument error: {0}\n", e.errorCode);
             }
 
-            return 0;
+
         }
 
-        public static Dictionary<string, string> CheckArgs(string[] args)
+        private static void executeApplication(bool logging, int port, String directory)
         {
-
-            Dictionary<string, string> flags = new Dictionary<string, string>();
-
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i].StartsWith("-") && args[i].Length == 2 && Char.IsLetter(args[i][1]))
-                {
-
-                    if (i + 1 < args.Length)
-                    {
-                        //to mmay kolejny 
-                        if (!(args[i + 1].StartsWith("-") && args[i + 1].Length == 2 && Char.IsLetter(args[i + 1][1])))
-                        {
-                            //czyli nastepny nie jest flaga
-                            flags.Add(args[i], args[i + 1]);
-                            i++;
-                        }
-                        else
-                        {
-                            flags.Add(args[i], null);
-                        }
-
-                    }
-
-                }
-            }
-            return flags;
-
+            Console.WriteLine("logging is {0}, port:{1}, directory:{2}\n", logging, port, directory);
         }
 
     }
